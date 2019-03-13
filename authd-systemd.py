fd9e36490b340602182a476fda40bd367892d7a5
Gabriel Bentley <gbentley@24hourfit.com>
2019-03-13 15:03:05 -0700

    modified:   authd-systemd.py

--- authd-systemd.py
+++ authd-systemd.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python
 # This is a python script that will handle starting/stopping of authd
 # for systemd.
-from os import remove,path,kill
+from os import remove, kill
 from time import sleep
 from re import search, match
 from sys import argv
 from subprocess import Popen,PIPE
 
-# This function starts the ossec-authd daemon by making a system call. The 
-# path variables define command line arguments for the ossec authd daemon.  
+# This function starts the ossec-authd daemon by making a system call. The
+# path variables define command line arguments for the ossec authd daemon. 
+
+
 def AuthdStart():
-    key_path = PATH_TO_OSSEC_SSL_KEY
-    cert_path = PATH_TO_OSSEC_SSL_CERT
+    key_path = 'PATH_TO_OSSEC_SSL_KEY'
+    cert_path = 'PATH_TO_OSSEC_SSL_CERT'
     authd_log = open('/tmp/authd.log', 'w+b')
     authd_start = ['/var/ossec/bin/ossec-authd', '-n', '-i', '-x', cert_path,
-                   '-k', key_path] 
+                   '-k', key_path]
     print('Starting ossec-authd...')
-    ossec_authd = Popen(authd_start, stdout=authd_log, stderr=authd_log)
+    Popen(authd_start, stdout=authd_log, stderr=authd_log)
     for proc in Popen(['ps', 'aux'], stdout=PIPE).stdout:
         if search('ossec-authd', proc):
             ossec_authd_pid = proc.split()[1]
@@ -35,7 +37,7 @@ def AuthdStop():
     kill(int(authd_pid), 15)
     remove('/tmp/authd.log')
 
-    
+
 # This function checks on the status of the ossec-authd daemon running as
 # a service by checking to see if the authd log file exists (which is
 # created when authd starts) and checks whether or not a process that
